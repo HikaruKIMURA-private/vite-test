d116bf3e313db21454f39ea7870fe9a9f3c486dc
木村輝 <hikaru@kimuraakiranoMacBook-Air.local>
2024-09-10 22:58:26 +0900

    カウンターの見た目に変更を加える

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -21,7 +21,7 @@ const Counter: React.FC<CounterProps> = ({ initialCount = 0 }) => {
 
   return (
     <div>
-      <h2>Count: {count}</h2>
+      <h2 style={{ color: "red", fontSize: "20px" }}>Count: {count}</h2>
       <button onClick={decrement}>ー</button>
       <button onClick={increment}>＋</button>
     </div>
